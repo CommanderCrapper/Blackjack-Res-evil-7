fc7d30ea577870046813b4e6ea755d2f8122536e
Commander Crapper <118539707+CommanderCrapper@users.noreply.github.com>
2023-04-17 10:14:28 -0400

    Beginning work

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
+//The value of each individual card, gets added up into the value
+let playerHand = [];
+let botHand = [];
 
+//The entire deck, numbered 1-11
+let deck = [1,2,3,4,5,6,7,8,9,10,11];
+
+//The value of all the cards
+let playerValue;
+let botValue;
+
+//Either active (hit last) or inactive (stayed)
+let playerStatus;
+let botStatus;
+
+//Look up chances for trump card to be drawn, then add code that adds a trump card to the player's inventory then draws a card
+
+function addPlayer() {
+    //Psuedo code, probably
+    //let newCard = random.num(deck);
+}
+
+function addBot() {
+
+}
+
+function endGame() {
+
+}
+
+function addTrump() {
+
+}
+
+function removeLast() {
+
+}
+
+function removeOppLast() {
+
+}
+
+function draw3() {
+
+}
+
+function draw4() {
+
+}
+
+function draw5() {
+
+}
+
+function draw6() {
+
+}
